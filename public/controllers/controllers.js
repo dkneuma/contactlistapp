debc20c62bd51706115a5b6670a3d6bd177d0421
Daniel Neumann <dkneuma@gmail.com>
2015-04-27 01:18:14 -0400

    Added public

---
+++ public/controllers/controllers.js
@@ -0,0 +1,3 @@
+/**
+ * Created by daniel.neumann on 4/27/15.
+ */
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get('/',function (req, res) {
-    res.send('Hello from Express server.js');
-});
+app.use(express.static('public'));
 
 app.listen(3000);
 console.log("Server listening on 3000");
