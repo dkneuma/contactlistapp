cf8519db63c913de3fa65cb676b222eebd6dd76a
Daniel Neumann <dkneuma@gmail.com>
2015-05-10 20:19:57 -0400

    Experimenting with Pagination
    Doing some pagination work. This formula
    https://github.com/fdietz/recipes-with-angular-js-examples/tree/master/c
    hapter8/recipe2 uses some code that doesn’t quite work.
    First issue:
    Need to read data in before calculating length of array for pagination.
    Resolved by creating a variable $scope.listLength = 0 and populating it
    on refresh.
    Second issue:
    Error in ‘offset’ filter, again, something is null to start. Unresolved
    at this point.

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -3,13 +3,21 @@
  */
 var myApp = angular.module('myApp', []);
 
+myApp.filter('offset', function() {
+    return function(input, start) {
+        start = parseInt(start, 10);
+        return input.slice(start);
+    };
+});
+
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
-
+$scope.listLength = 0;
 var refresh = function () {
         $http.get('/contactlist').success(function (response) {
             console.log("I got the data I requested");
             $scope.contactlist = response;
+            $scope.listLength = $scope.contactlist.length;
             $scope.contact="";
         });
     };
@@ -46,6 +54,53 @@ $scope.update = function() {
     });
 
 };
+
+$scope.itemsPerPage = 5;
+$scope.currentPage = 0;
+
+$scope.range = function() {
+    var rangeSize = 5;
+    var ret = [];
+    var start;
+
+    start = $scope.currentPage;
+    if ( start > $scope.pageCount()-rangeSize ) {
+        start = $scope.pageCount()-rangeSize+1;
+    }
+
+    for (var i=start; i<start+rangeSize; i++) {
+        ret.push(i);
+    }
+    return ret;
+};
+
+$scope.prevPage = function() {
+    if ($scope.currentPage > 0) {
+        $scope.currentPage--;
+    }
+};
+
+$scope.prevPageDisabled = function() {
+    return $scope.currentPage === 0 ? "disabled" : "";
+};
+
+$scope.pageCount = function() {
+   // console.log("Called pageCount with length of: " + $scope.contactlist.length);
+    var retVal =  Math.ceil($scope.listLength/$scope.itemsPerPage)-1;
+   // console.log("Return value = " + retVal);
+    return retVal;
+};
+
+$scope.nextPage = function() {
+    if ($scope.currentPage < $scope.pageCount()) {
+        $scope.currentPage++;
+    }
+};
+
+$scope.nextPageDisabled = function() {
+    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+};
+
 
 
 }]);
