04302de2b0760b337f3fb643e9985046cc592b97
Daniel Neumann <dkneuma@gmail.com>
2015-05-20 13:31:01 -0400

    Updated GET and POST to mongoose

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require("express");
 var mongojs = require("mongojs");
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 //var db = mongojs('contactlist',['contactlist']);
-var db = mongojs("mongodb://admin:nimda~1@ds043329.mongolab.com:43329/contactlist",['contactlist']);
+//var db = mongojs("mongodb://admin:nimda~1@ds043329.mongolab.com:43329/contactlist",['contactlist']);
 //var contactlist = db.collection("contactlist");
+
+mongoose.connect('mongodb://localhost/contactlistapp', function(err) {
+   if(err) {
+       console.log('connection error', err);
+   } else {
+       console.log('connection successful');
+   }
+});
+
+var ContactListSchema = new mongoose.Schema({
+   Name: String,
+    Email: String,
+    Number: String
+});
+
+var ContactList = mongoose.model('ContactList', ContactListSchema);
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -14,19 +32,18 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/contactlist', function (req, res) {
     console.log("I received a GET request");
 
-
-    db.contactlist.find(function (err, docs) {
+    ContactList.find(function (err, docs) {
         console.log(docs);
         res.json(docs);
-
     });
 });
 
 app.post('/contactlist', function (req, res) {
     console.log("I received a POST request");
     console.log(req.body);
-    db.contactlist.insert(req.body, function(err, doc) {
-        res.json(doc);
+    ContactList.create(req.body, function(err, post){
+       if (err) return err;
+        res.json(post);
     });
 });
 
